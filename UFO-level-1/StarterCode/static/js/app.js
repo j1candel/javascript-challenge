74c2a793718fd5e3e85dc8c181cbc056b670fb4e
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 13:37:29 -0800

    Cleaning complete

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -10,14 +10,18 @@ let dateTime = d3.select("#datetime")
 let btnSearch = d3.select("#filter-btn")
 
 // Building the html table 
-let loadTableRows = (allData) => { 
+let loadTableRows = (data) => { 
     
+    // Erasing all previous rows 
     tBody.html("");
-	
-	allData.forEach(allRowData => { 
-		
+    
+    // adding all "data" to the table 
+	data.forEach(allRowData => { 
+        
+        // Adding a table row
 		let tableRow = tBody.append("tr");  
-		
+        
+        // Adding values in the table row
 		tableFields.forEach(field => tableRow.append("td").text(allRowData[field]))
 	});
 }
@@ -25,18 +29,24 @@ let loadTableRows = (allData) => {
 // Display the html table with all the rows
 loadTableRows(tableData);
 
+// Creating the search button and adding filter
 btnSearch.on("click", () => {
 
     // Create event handler
     d3.event.preventDefault();
 
+    // Give the input value a variable 
     let searchDateTime = dateTime.property("value");
 
+    // Filter the table data to only return data equal to the input variable 
     let filterTableDate = tableData.filter(tableData => tableData.datetime === searchDateTime);
 
+    // If the length of filterTableDate is longer than one row then the loadTableRows function begins
     if(filterTableDate.length !== 0 ){
         loadTableRows(filterTableDate)
     }
+
+    // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
     else{
         tBody.html("")
 
