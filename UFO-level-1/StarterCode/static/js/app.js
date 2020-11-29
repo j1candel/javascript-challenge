64f5520552d5aa1da5da9ef156a8e3a35c227a89
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 11:49:28 -0800

    beginning to filter

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,16 +1,38 @@
 // Selecting data from from data.js
-let sightings = data;
+let tableData = data;
+
+
+let tableFields= ["datetime", "city","ar","country","shape","durationMinutes","comments"]
+
+let tBody = d3.select("tbdoy");
+let datetime = d3.select("#datetime")
+let btnSearch = d3.select("#filter-btn")
+
+
+let newTableRows = (filterData) => {
+
+    tbody= ""
+}
+/*//Selecting the button 
+let button = d3.select("#button");
 
 //Selecting the dateTime 
-let dateTime = d3.select("#datetime")
+let form = d3.select("#form");
 
 //Create an event handler
-dateTime.on("submit", runEnter);
-
+button.on("click", runEnter);
+button.on("submit", runEnter);
 
 // Completing the event handler form
 function runEnter() {
     
     //Prevent the page from refreshing 
     d3.event.preventDefault()
-}
+
+    let inputElement = d3.select("#datetime")
+
+    let inputValue = inputElement.property("value")
+
+    console.log(inputValue)
+    console.log(sightings)
+}*/
