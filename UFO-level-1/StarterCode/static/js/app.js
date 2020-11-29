4be8d3e9e33cafaff231daf95dae710b1536e825
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 12:35:31 -0800

    data revealed on sight

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -4,35 +4,22 @@ let tableData = data;
 
 let tableFields= ["datetime", "city","ar","country","shape","durationMinutes","comments"]
 
-let tBody = d3.select("tbdoy");
+let tbody = d3.select("tbody");
 let datetime = d3.select("#datetime")
 let btnSearch = d3.select("#filter-btn")
 
 
-let newTableRows = (filterData) => {
-
-    tbody= ""
-}
-/*//Selecting the button 
-let button = d3.select("#button");
-
-//Selecting the dateTime 
-let form = d3.select("#form");
-
-//Create an event handler
-button.on("click", runEnter);
-button.on("submit", runEnter);
-
-// Completing the event handler form
-function runEnter() {
+let loadTableRows = (allData) => { 
     
-    //Prevent the page from refreshing 
-    d3.event.preventDefault()
+    tbody.html("");
+	
+	allData.forEach(allRowData => { 
+		
+		let tableRow = tbody.append("tr");  
+		
+		tableFields.forEach(field => tableRow.append("td").text(allRowData[field]))
+	});
+}
 
-    let inputElement = d3.select("#datetime")
 
-    let inputValue = inputElement.property("value")
-
-    console.log(inputValue)
-    console.log(sightings)
-}*/
+loadTableRows(tableData);
