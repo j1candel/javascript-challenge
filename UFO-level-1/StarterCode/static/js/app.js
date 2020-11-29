5afff91daab4cdc0b152abc1fc1ac0184effbe3e
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 12:40:27 -0800

    Adding filter

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -4,18 +4,18 @@ let tableData = data;
 
 let tableFields= ["datetime", "city","ar","country","shape","durationMinutes","comments"]
 
-let tbody = d3.select("tbody");
-let datetime = d3.select("#datetime")
+let tBody = d3.select("tbody");
+let dateTime = d3.select("#datetime")
 let btnSearch = d3.select("#filter-btn")
 
 
 let loadTableRows = (allData) => { 
     
-    tbody.html("");
+    tBody.html("");
 	
 	allData.forEach(allRowData => { 
 		
-		let tableRow = tbody.append("tr");  
+		let tableRow = tBody.append("tr");  
 		
 		tableFields.forEach(field => tableRow.append("td").text(allRowData[field]))
 	});
@@ -23,3 +23,12 @@ let loadTableRows = (allData) => {
 
 
 loadTableRows(tableData);
+
+btnSearch.on("click", () => {
+
+    d3.event.preventDefault();
+
+    let searchDateTime = dateTime.property("value")
+
+    let filteredTableDate = tableData.filter(tableData => tableData.dateTime === searchDateTime)
+})
