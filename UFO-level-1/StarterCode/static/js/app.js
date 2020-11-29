596349aa81734247326c3e4ae1d4ef1af63f3ed4
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 13:07:45 -0800

    Cleaning code

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,14 +1,15 @@
 // Selecting data from from data.js
 let tableData = data;
 
-
+// Making an array with all the names of the fields 
 let tableFields= ["datetime", "city","ar","country","shape","durationMinutes","comments"]
 
+// Obtaining the HTML reference 
 let tBody = d3.select("tbody");
 let dateTime = d3.select("#datetime")
 let btnSearch = d3.select("#filter-btn")
 
-
+// Building the html table 
 let loadTableRows = (allData) => { 
     
     tBody.html("");
@@ -21,16 +22,15 @@ let loadTableRows = (allData) => {
 	});
 }
 
-
+// Display the html table with all the rows
 loadTableRows(tableData);
 
 btnSearch.on("click", () => {
 
+    // Create event handler
     d3.event.preventDefault();
 
     let searchDateTime = dateTime.property("value");
-
-
 
     let filterTableDate = tableData.filter(tableData => tableData.datetime === searchDateTime);
 
