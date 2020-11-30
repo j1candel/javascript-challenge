a2b5902b5c34adc0c647df06448b9ab41b69770b
Jovany Candelario <jovany.candel@gmail.com>
2020-11-29 17:39:55 -0800

    finished second filter

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -6,8 +6,13 @@ let tableFields= ["datetime", "city","state","country","shape","durationMinutes"
 
 // Obtaining the HTML reference 
 let tBody = d3.select("tbody");
-let dateTime = d3.select("#datetime")
-let btnSearch = d3.select("#filter-btn")
+let btnSearch = d3.select("#btnSearch")
+
+let dateTime = d3.select("#inputDateTime")
+let city = d3.select("#inputCity")
+let state = d3.select("#inputState")
+let country = d3.select("#inputCountry")
+let shape = d3.select("#inputShape")
 
 // Building the html table 
 let loadTableRows = (data) => { 
@@ -37,19 +42,89 @@ btnSearch.on("click", () => {
 
     // Give the input value a variable 
     let searchDateTime = dateTime.property("value");
+    let searchCity = city.property("value");
+    let searchState = state.property("value");
+    let searchCountry = country.property("value");
+    let searchShape = shape.property("value");
+    
+        // Filter the table data to only return data equal to the input variable 
+        if(searchDateTime){
+            let filterTableDate = tableData.filter(tableData => tableData.datetime === searchDateTime);
 
-    // Filter the table data to only return data equal to the input variable 
-    let filterTableDate = tableData.filter(tableData => tableData.datetime === searchDateTime);
+            // If the length of filterTableDate is longer than one row then the loadTableRows function begins
+            if(filterTableDate.length !== 0 ){
+                loadTableRows(filterTableDate)
+            }
 
-    // If the length of filterTableDate is longer than one row then the loadTableRows function begins
-    if(filterTableDate.length !== 0 ){
-        loadTableRows(filterTableDate)
-    }
+            // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
+            else{
+                tBody.html("")
 
-    // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
-    else{
-        tBody.html("")
+                tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date");
+            }
+        }
 
-        tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date")
-    }
-})
+        else if(searchCity){
+            let filterTableDate = tableData.filter(tableData => tableData.city === searchCity);
+
+            // If the length of filterTableDate is longer than one row then the loadTableRows function begins
+            if(filterTableDate.length !== 0 ){
+                loadTableRows(filterTableDate)
+            }
+
+            // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
+            else{
+                tBody.html("")
+
+                tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date");
+            }
+        }
+        
+        else if(searchState){
+            let filterTableDate = tableData.filter(tableData => tableData.state === searchState);
+
+            // If the length of filterTableDate is longer than one row then the loadTableRows function begins
+            if(filterTableDate.length !== 0 ){
+                loadTableRows(filterTableDate)
+            }
+
+            // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
+            else{
+                tBody.html("")
+
+                tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date");
+            }
+        }
+
+        else if(searchCountry){
+            let filterTableDate = tableData.filter(tableData => tableData.country === searchCountry);
+
+            // If the length of filterTableDate is longer than one row then the loadTableRows function begins
+            if(filterTableDate.length !== 0 ){
+                loadTableRows(filterTableDate)
+            }
+
+            // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
+            else{
+                tBody.html("")
+
+                tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date");
+            }
+        }
+
+        else{
+            let filterTableDate = tableData.filter(tableData => tableData.shape === searchShape);
+
+            // If the length of filterTableDate is longer than one row then the loadTableRows function begins
+            if(filterTableDate.length !== 0 ){
+                loadTableRows(filterTableDate)
+            }
+
+            // If the lenth of filterTableDate is equal 0 then they delete the table and display the text 
+            else{
+                tBody.html("")
+
+                tBody.append("tr").append("td").text("Unfortunately, there were no sightings on that date");
+            }
+        }
+        })
